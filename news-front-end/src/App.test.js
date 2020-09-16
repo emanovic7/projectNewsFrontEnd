2b9e8a9987130ec742fe8a0cc44c51132a9d8359
emanovic7 <e_owusu@live.com>
2020-09-16 18:11:30 -0400

    cleans up test code

--- news-front-end/src/App.test.js
+++ news-front-end/src/App.test.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import App from './App';
 
+// enzyme's react adapter setup
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders react app', () => {
-  const wrapper = shallow(<App />);
+/**
+  Factory function to create a shallowWrapper for the App component
+  *@function testSetup 
+  *@returns { ShallowWrapper }
+*/
+const testSetup = () => shallow(<App />);
+
+const findByAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+}
+
+//tests 
+test('renders without error', () => {
+  const wrapper = testSetup();
+  const appComponent = findByAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
 });
+
+test('renders heading', () => {
+  const wrapper = testSetup();
+  const header = findByAttr(wrapper, 'title-header');
+  expect(header.length).toBe(1);
+})
